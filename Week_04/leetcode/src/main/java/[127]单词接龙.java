1451d9ff66d799a27a0111f041d1a1ae2ac4ffc8
GuangZhaoPan <153952402@qq.com>
2021-01-24 19:14:17 +0800

    第四周作业提交

---
+++ Week_04/leetcode/src/main/java/[127]单词接龙.java
@@ -0,0 +1,113 @@
+//字典 wordList 中从单词 beginWord 和 endWord 的 转换序列 是一个按下述规格形成的序列： 
+//
+// 
+// 序列中第一个单词是 beginWord 。 
+// 序列中最后一个单词是 endWord 。 
+// 每次转换只能改变一个字母。 
+// 转换过程中的中间单词必须是字典 wordList 中的单词。 
+// 
+//
+// 给你两个单词 beginWord 和 endWord 和一个字典 wordList ，找到从 beginWord 到 endWord 的 最短转换序列 中
+//的 单词数目 。如果不存在这样的转换序列，返回 0。 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","lo
+//g","cog"]
+//输出：5
+//解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","lo
+//g"]
+//输出：0
+//解释：endWord "cog" 不在字典中，所以无法进行转换。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= beginWord.length <= 10 
+// endWord.length == beginWord.length 
+// 1 <= wordList.length <= 5000 
+// wordList[i].length == beginWord.length 
+// beginWord、endWord 和 wordList[i] 由小写英文字母组成 
+// beginWord != endWord 
+// wordList 中的所有字符串 互不相同 
+// 
+// Related Topics 广度优先搜索 
+// 👍 684 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.List;
+
+class WordLadderSolution{
+    
+    public static void main(String[] args) {
+        Solution solution = new WordLadderSolution().new Solution();
+    }
+    
+    
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (wordList == null || wordList.size() == 0) return 0;
+        //hashset的好处：去重也完成了
+        //开始端
+        HashSet<String> start = new HashSet<>();
+        //结束端
+        HashSet<String> end = new HashSet<>();
+        //所有字符串的字典
+        HashSet<String> dic = new HashSet<>(wordList);
+        start.add(beginWord);
+        end.add(endWord);
+        if (!dic.contains(endWord)) return 0;
+        //经历过上面的一系列判定，到这里的时候，若是有路径，则最小是2，所以以2开始
+        return bfs(start, end, dic, 2);
+    }
+
+    public int bfs(HashSet<String> st, HashSet<String> ed, HashSet<String> dic, int l) {
+        //双端查找的时候，若是有任意一段出现了“断裂”，也就是说明不存在能够连上的路径，则直接返回0
+        if (st.size() == 0) return 0;
+        if (st.size() > ed.size()) {//双端查找，为了优化时间，永远用少的去找多的，比如开始的时候塞进了1000个，而结尾只有3个，则肯定是从少的那一端开始走比较好
+            return bfs(ed, st, dic, l);
+        }
+        //BFS的标记行为，即使用过的不重复使用
+        dic.removeAll(st);
+        //收集下一层临近点
+        HashSet<String> next = new HashSet<>();
+        for (String s : st) {
+            char[] arr = s.toCharArray();
+            for (int i = 0; i < arr.length; i++) {
+                char tmp = arr[i];
+                //变化
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if (tmp == c) continue;
+                    arr[i] = c;
+                    String nstr = new String(arr);
+                    if (dic.contains(nstr)) {
+                        if (ed.contains(nstr)) return l;
+                        else next.add(nstr);
+                    }
+                }
+                //复原
+                arr[i] = tmp;
+            }
+        }
+        return bfs(next, ed, dic, l + 1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+}
