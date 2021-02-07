adef71a485f740af84744d7556ec129b6813c7f2
GuangZhaoPan <153952402@qq.com>
2021-02-07 21:42:18 +0800

    第六周作

---
+++ Week_06/leetcode/src/main/java/[221]最大正方形.java
@@ -0,0 +1,92 @@
+//在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"]
+//,["1","0","0","1","0"]]
+//输出：4
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [["0","1"],["1","0"]]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：matrix = [["0"]]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 300 
+// matrix[i][j] 为 '0' 或 '1' 
+// 
+// Related Topics 动态规划 
+// 👍 658 👎 0
+
+
+package leetcode.editor.cn;
+class MaximalSquareSolution{
+    
+    public static void main(String[] args) {
+        Solution solution = new MaximalSquareSolution().new Solution();
+    }
+    
+    
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+    class Solution_1_1 {
+        public int maximalSquare(char[][] matrix) {
+            if(matrix.length == 0) return 0;
+            int answer = 0;
+            int[][] dp = new int[matrix.length][matrix[0].length];
+            for(int i = 0; i < matrix.length - 1; i++) {
+                for(int j = 0; j < matrix[0].length - 1; j++) {
+                    if(i == 0 || j == 0) dp[i][j] = matrix[i][j];
+                    else if(matrix[i][j] == 1) {
+                        dp[i][j] = Math.min(Math.min(dp[i][j], dp[i - 1][j]), dp[i][j - 1]) + 1;
+                    };
+                    answer = Math.max(dp[i][j], answer);
+                }
+            }
+            return answer;
+        }
+    }
+    
+}
